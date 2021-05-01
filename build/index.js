8ff04ab2aaf8a3a9f42f05ce920541fd2a6d0615
Kartik <kartik@hyperverge.co>
2021-05-01 22:19:11 +0530

    Fix: indentation via prettier

--- build/index.js
+++ build/index.js
@@ -1,9 +1,11 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const table_1 = __importDefault(require("./sql/table"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const table_1 = __importDefault(require('./sql/table'));
 const table = new table_1.default('user', { name: 'string', age: 'int' });
 table.insert({ name: 'Kartik ', age: 12 });
 table.insert({ name: 'rama', age: 1024 });
--- src/sql/table.ts
+++ src/sql/table.ts
@@ -20,32 +20,35 @@ export default class Table {
   }
 
   private verifyInputSchema(input): boolean {
-      if(Object.keys(input).length !== Object.keys(this.schema).length) {
-          throw new Error('Invalid input schema');
+    if (Object.keys(input).length !== Object.keys(this.schema).length) {
+      throw new Error('Invalid input schema');
+    }
+    Object.keys(input).forEach((key) => {
+      if (!Object.keys(this.schema).includes(key)) {
+        throw new Error(`${key} isn't a valid key for table ${this.name}`);
       }
-      Object.keys(input).forEach(key => {
-        if(!Object.keys(this.schema).includes(key)) {
-            throw new Error(`${key} isn't a valid key for table ${this.name}`);
-        }
-      })
-      return true;
+    });
+    return true;
   }
 
   private verifyInput(input): boolean {
-      this.verifyInputSchema(input);
-      Object.keys(input).forEach(key => {
-          if(this.schema[key] === 'string') {
-              if(input[key].length > 20) throw new Error('String input must be at most 20 characters');
-          } else {
-              if(typeof input[key] !== 'number') throw new Error('Integer input must be of type number');
-              if(input[key] < -1024 || input[key] > 1024) throw new Error('Integer input must be between 1024 and -1024');
-          }
-      })
-      return true;
+    this.verifyInputSchema(input);
+    Object.keys(input).forEach((key) => {
+      if (this.schema[key] === 'string') {
+        if (input[key].length > 20)
+          throw new Error('String input must be at most 20 characters');
+      } else {
+        if (typeof input[key] !== 'number')
+          throw new Error('Integer input must be of type number');
+        if (input[key] < -1024 || input[key] > 1024)
+          throw new Error('Integer input must be between 1024 and -1024');
+      }
+    });
+    return true;
   }
 
   public insert(input): void {
-      this.verifyInput(input);
-      this.table.push(input);
+    this.verifyInput(input);
+    this.table.push(input);
   }
 }
